8b5e094641237e7fb1e92672291f57d5da2abe02
bucciax <bucciarelligianluca@gmail.com>
2023-07-28 18:02:49 +0200

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,11 +9,12 @@ export interface Platform {
 }
 
 export interface Game {
-    id: number;
-    name: string;
+    id: number
+    name: string
     background_image: string
     parent_platforms: { platform: Platform }[]
     metacritic: number
+    rating_top: number
   }
 
 const useGames = (gameQuery: GameQuery) => 
